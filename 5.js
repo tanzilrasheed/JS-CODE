70e825a24c858938b9c0479ec131a8e10652ac2b
tanzilrasheed <161126036+tanzilrasheed@users.noreply.github.com>
2024-04-03 11:19:49 +0530

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,11 +1,23 @@
+//prime number
+
 const input = require('readline-sync');
-let num = input.questionInt('enter a number: '), f = 2;
-while (num % f != 0) {
-  f++;
+let x = input.questionInt('enter: ');
+function prime(x)
+{
+    for (let i = 2; i <= Math.sqrt(x); i++)
+    {
+        if (x % i == 0)
+        {
+            return 'not prime number';
+        }
+    }
+    if(x > 1)
+    {
+        return 'prime number';
+    }
+    else
+    {
+        return 'not prime number';
+    }
 }
-if (num == f) {
-  console.log('yes, it\'s a prime number');
-}
-else {
-  console.log('not a prime number');
-}
+console.log(prime(x));
